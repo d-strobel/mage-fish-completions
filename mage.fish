90ed63fa932b72b42dd960aeaec6bee478b98292
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2024-06-10 00:15:04 +0200

    feat: add fish completion script

---
+++ mage.fish
@@ -0,0 +1,25 @@
+# fish completion for mage                               -*- shell-script -*-
+
+# this function handles the mage targets and descriptions
+function __fish_mage_targets
+  # get targets from mage
+  set -l targets (mage -l)
+
+  # loop over targets and skip the first line
+  for target in $targets[2..]
+    # first trim leading and trailing whitespaces
+    # then split the target into two parts on the first space
+    set -l split (string split -m 1 ' ' (string trim $target))
+
+    # format the output as a fish completion
+    # first string is the command, second is the description
+    printf %s\t%s\n $split[1] $split[2]
+  end
+end
+
+# function to get completions for mage
+function __fish_mage_completions
+  __fish_mage_targets
+end
+
+complete -c mage -f -a "(__fish_mage_completions)"
